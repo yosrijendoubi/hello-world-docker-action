7bcd070fff0e3d787188b8c8046dd7d2d60768f1
root <yosri.jendoubi@esprit.tn>
2022-02-28 21:44:15 +0100

    testing

---
+++ main.tf
@@ -0,0 +1,44 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "2.96.0"
+    }
+  }
+ backend "azurerm" {
+    subscription_id      = "47bb1c58-ee41-477f-9ba8-ef0d878cfb29"
+    storage_account_name = "yosristorage5"
+    container_name       = "edge"
+    key                  = "terraform.tfstate"
+    resource_group_name  = "iotStorageYosri"
+  }
+  
+}
+
+
+provider "azurerm" {
+  features {}
+
+}
+
+resource "azurerm_resource_group" "pipelinerg"{
+  name="pipelinerg"
+  location="West Europe"
+}
+
+
+resource "azurerm_storage_account" "pipelinesa" {
+  name                     = "pipelinesa2"
+  resource_group_name      = azurerm_resource_group.pipelinerg.name
+  location                 = azurerm_resource_group.pipelinerg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+  allow_blob_public_access = true
+}
+
+# Here we are creating a container in the storage account
+resource "azurerm_storage_container" "pipelinecontainer2" {
+  name                  = "pipelinecontainer2"
+  storage_account_name  = azurerm_storage_account.pipelinesa.name
+  container_access_type = "private"
+}
